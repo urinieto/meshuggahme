10d9c8171162b9b445b3fc697b0518dd75da4ad3
kmarutyan <kmarutyan@pandora.com>
2015-11-18 16:01:43 -0800

    more js and css

--- web/javascripts/jsscript.js
+++ web/javascripts/jsscript.js
@@ -3,6 +3,21 @@ $( document ).ready(function() {
     console.log( "Page is loaded!" );
 
     $('#featured-videos-carousel').on('slide.bs.carousel', function () {
-        console.log( "Carousel is moved!" );
+        //if($(".featured-li").hasClass('active'))   {
+        //     console.log( "Carousel event!" );
+        //    $('.embedded-video').each(function( index ) {
+        //        var url =$(this).attr('src');
+        //        $('#this').attr('src', '');
+        //        console.log( url );
+        //        $(this).attr('src', url);
+        //        //$(this).attr('src', $('iframe').attr('src'));
+        //    });
+        //}
+
+
+
+
     })
+
+
 });
