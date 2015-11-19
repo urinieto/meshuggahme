af7ee19a8d56c97dab4a7fb9a6b02db40ef9851d
Jeff Frasca <jfrasca@pandora.com>
2015-11-19 10:43:36 -0800

    Turn muxer start script into init script

--- muxer/muxer_service.init.sh
+++ muxer/muxer_service.init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+ACTION=$1
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd $REPO_ROOT/muxer
+
 if [ -e meshuggahme.default ]; then
     source meshuggahme.default
 else
@@ -14,12 +20,33 @@ if [ "$MESHUGGAHME_VENV" ]; then
     source $MESHUGGAHME_VENV/bin/activate
 fi
 
-REPO_ROOT=$(git rev-parse --show-toplevel)
+function meshuggahme_start() {
+    if [ "$MESHUGGAHME_USE_GUNICORN" ]; then
+        $MESHUGGAHME_VENV/bin/gunicorn -D -b 127.0.0.1:5000 -w 4 meshuggahme_muxer:app
+    else
+        python meshuggahme_muxer.py 
+    fi
+}
 
-cd $REPO_ROOT/muxer
+function meshuggahme_stop() {
+    if [ "$MESHUGGAHME_PIDFILE" ]; then
+        kill $(cat $MESHUGGAHME_PIDFILE)
+    fi
+}
 
-if [ "$MESHUGGAHME_USE_GUNICORN" ]; then
-    $MESHUGGAHME_VENV/bin/gunicorn -D -b 127.0.0.1:5000 -w 4 meshuggahme_muxer:app
-else
-    python meshuggahme_muxer.py 
-fi
+case "$ACTION" in
+    start)
+        meshuggahme_start
+        ;;
+    stop)
+        meshuggahme_stop
+        ;;
+    restart)
+        meshuggahme_stop
+        meshuggahme_start
+        ;;
+    *)
+        echo "It's an init script.  Specify start/stop/restart."
+        exit 1
+        ;;
+esac
