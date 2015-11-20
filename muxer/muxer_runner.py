0d34c70541b0f4991cff28ef4ac78dc323cfac16
Jeff Frasca <jfrasca@pandora.com>
2015-11-20 01:03:21 -0800

    Fix sleep/retry loop behavior

--- muxer/muxer_runner.py
+++ muxer/muxer_runner.py
@@ -23,51 +23,51 @@ def muxer_worker():
     while True:
         try:
             yt_url = redis.lpop('yturls')
-            if not yt_url:
+            if yt_url is None:
                 time.sleep(10)
-                next
-            m = Muxer(yt_url=yt_url)
-            m.download_video()
-            with open(
-                '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
-            ) as f:
-                f.write('{"status": "processing", "stage": 1}')
-            m.demux()
-            with open(
-                '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
-            ) as f:
-                f.write('{"status": "processing", "stage": 2}')
-            m.convert_to_wav()
-            with open(
-                '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
-            ) as f:
-                f.write('{"status": "processing", "stage": 3}')
-            # XXX: Call meshuggahfier here, and use its output in place of m.get_audio_file() 
-            meshuggahfied_file = '{output_path}/{ytid}mm.wav'.format(
-                output_path=m.output_dir, ytid=m.ytid
-            )
-            if not os.path.exists(meshuggahfied_file):
-                meshuggahme(
-                    m.get_audio_file(), 
-                    X, improve_func=improve_normal,
-                    onset_dicts=onset_dicts, onset_dir=onset_dir,
-                    metric='correlation', output_file=meshuggahfied_file,
-                    original_w=6.5
+            else:
+                m = Muxer(yt_url=yt_url)
+                m.download_video()
+                with open(
+                    '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
+                ) as f:
+                    f.write('{"status": "processing", "stage": 1}')
+                m.demux()
+                with open(
+                    '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
+                ) as f:
+                    f.write('{"status": "processing", "stage": 2}')
+                m.convert_to_wav()
+                with open(
+                    '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
+                ) as f:
+                    f.write('{"status": "processing", "stage": 3}')
+                # XXX: Call meshuggahfier here, and use its output in place of m.get_audio_file() 
+                meshuggahfied_file = '{output_path}/{ytid}mm.wav'.format(
+                    output_path=m.output_dir, ytid=m.ytid
                 )
-            with open(
-                '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
-            ) as f:
-                f.write('{"status": "processing", "stage": 4}')
-            meshuggahfied_file = m.compress_wav(meshuggahfied_file)
-            with open(
-                '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
-            ) as f:
-                f.write('{"status": "processing", "stage": 5}')
-            m.remux(meshuggahfied_file).split('/')[-1]
-            with open(
-                '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
-            ) as f:
-                f.write('{"status": "complete"}')
+                if not os.path.exists(meshuggahfied_file):
+                    meshuggahme(
+                        m.get_audio_file(), 
+                        X, improve_func=improve_normal,
+                        onset_dicts=onset_dicts, onset_dir=onset_dir,
+                        metric='correlation', output_file=meshuggahfied_file,
+                        original_w=6.5
+                    )
+                with open(
+                    '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
+                ) as f:
+                    f.write('{"status": "processing", "stage": 4}')
+                meshuggahfied_file = m.compress_wav(meshuggahfied_file)
+                with open(
+                    '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
+                ) as f:
+                    f.write('{"status": "processing", "stage": 5}')
+                m.remux(meshuggahfied_file).split('/')[-1]
+                with open(
+                    '{output_dir}/{ytid}.status.json'.format(output_dir=output_dir, ytid=m.ytid), 'w'
+                ) as f:
+                    f.write('{"status": "complete"}')
         except Exception as e:
             print repr(e)
 
